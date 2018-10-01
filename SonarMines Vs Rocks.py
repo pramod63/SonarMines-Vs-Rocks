42e3a1b65a7fecebaf0d8997a206736deb46d83f
Pramod Naik <38611348+pramod63@users.noreply.github.com>
2018-10-02 02:24:04 +0530

    Add files via upload

---
+++ SonarMines Vs Rocks.py
@@ -0,0 +1,219 @@
+# Load libraries
+
+import numpy
+from pandas import read_csv
+from pandas import set_option
+from matplotlib import pyplot
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+
+# Load Dataset
+dataset = read_csv('Sonar Vs Rock.csv', header=None)
+# Shape
+print(dataset.shape)
+# Types
+set_option('display.max_rows', 500)
+print(dataset.dtypes)
+# Head
+set_option('display.width', 100)
+print(dataset.head(20))
+# Description Change the precision to 3 decimal place
+set_option('precision', 3)
+print(dataset.describe())
+# class distribution
+print(dataset.groupby(60).size())
+# Histogram
+dataset.hist(sharex=False, sharey=False, xlabelsize=1, ylabelsize=1)
+pyplot.show()
+# density
+dataset.plot(kind='density', subplots=True, layout=(8, 8), sharex=False,
+             legend=False, fontsize=1)
+pyplot.show()
+# Box and Whisker plots
+dataset.plot(kind='box', subplots=True, layout=(8, 8), sharex=False,
+             sharey=False, fontsize=1)
+pyplot.show()
+# Multimodal data visualization
+# correlation matrix
+fig = pyplot.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(dataset.corr(), vmin=-1, vmax=1, interpolation='none')
+fig.colorbar(cax)
+pyplot.show()
+
+
+# Encoding the categorical data and Independent variable
+array = dataset.values
+X = array[:, 0:60].astype(float)
+Y = array[:, 60]
+labelencoder_Y = LabelEncoder()
+array[:, 60] = labelencoder_Y.fit_transform(array[:, 60])
+onehotencoder = OneHotEncoder(categorical_features=[60])
+Y = onehotencoder.fit_transform(Y).toarray()
+Y1 = Y.astype(int)
+
+# Split-out validation dataset
+seed = 7
+validation_size = 0.20
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y1, test_size=validation_size, random_state=seed)
+
+# Test option and evaluation metric
+num_folds = 10
+seed = 7
+scoring = 'accuracy'
+
+# Spot-check algorithm
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeRegressor()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+
+results = []
+names = []
+for name, model in models:
+    kfold = KFold(n_splits=num_folds, random_state=seed)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold,
+                                 scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)"  % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+# compare algorithm
+fig = pyplot.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+pyplot.boxplot(results)
+ax.set_xticklabels(names)
+pyplot.show()
+
+# Standardize the dataset
+pipelines = []
+pipelines.append(('ScaledLR',Pipeline([('Scaler', StandardScaler()),
+                                       ('LR', LogisticRegression())])))
+pipelines.append(('ScaledDA',
+                  Pipeline([('Scaler', StandardScaler()),
+                            ('LDA', LinearDiscriminantAnalysis())])))
+pipelines.append(('ScaledKNN',
+                  Pipeline([('Scaler', StandardScaler()),
+                            ('KNN', KNeighborsClassifier())])))
+pipelines.append(('ScaledCART',
+                  Pipeline([('Scaler', StandardScaler()),
+                            ('CART', DecisionTreeRegressor())])))
+pipelines.append(('ScaledNB',
+                  Pipeline([('Scaler', StandardScaler()),
+                            ('NB', GaussianNB())])))
+pipelines.append(('ScaledSVM',
+                  Pipeline([('Scaler', StandardScaler()),
+                            ('SVM', SVC())])))
+results = []
+names = []
+for name, model in pipelines:
+    kfold = KFold(n_splits=num_folds, random_state=seed)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s : %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+# Compare algorithm
+fig = pyplot.figure()
+fig.suptitle('Scaled Algorithm Comaprison')
+ax = fig.add_subplot(111)
+pyplot.boxplot(results)
+ax.set_xticklabels(names)
+pyplot.show()
+
+# Tune Scaled KNN
+scaler = StandardScaler().fit(X_train)
+rescaledX = scaler.transform(X_train)
+neighbors = [1,3,5,7,9,11,13,15,17,19,21]
+param_grid = dict(n_neighbors=neighbors)
+model = KNeighborsClassifier()
+kfold = KFold(n_splits=num_folds, random_state=seed)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result = grid.fit(rescaledX, Y_train)
+print("Best: %f using %s " % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f)  with: %r" % (mean, stdev, param))
+
+# Tune scaled SVM
+scaler = StandardScaler().fit(X_train)
+rescaledX = scaler.transform(X_train)
+c_values = [0.1, 0.3, 0.5, 0.7, 0.9, 1.0, 1.3, 1.5, 1.7, 2.0]
+kernel_values = ['linear', 'poly', 'rbf', 'sigmoid']
+param_grid = dict(C=c_values, kernel=kernel_values)
+model =SVC()
+kfold = KFold(n_splits=num_folds, random_state=seed)
+grid = GridSearchCV(estimator=model, scoring=scoring, param_grid=param_grid, cv=kfold)
+grid_result = grid.fit(rescaledX, Y_train)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with : %r" % (mean, stdev, param))
+    
+# Ensembles
+ensembles = []
+ensembles.append(('AB', AdaBoostClassifier()))
+ensembles.append(('GBM', GradientBoostingClassifier()))
+ensembles.append(('RF', RandomForestClassifier()))
+ensembles.append(('ET', ExtraTreesClassifier()))
+results = []
+names = []
+for name, model in ensembles:
+    kfold = KFold(n_splits=num_folds, random_state=seed)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s : %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+    
+# Compare algorithms
+fig = pyplot.figure()
+fig.suptitle('Ensemble Algorithm Comparison')
+ax = fig.add_subplot(111)
+pyplot.boxplot(results)
+ax.set_xticklabels(names)
+pyplot.show()
+
+# Prepare the model
+scaler = StandardScaler().fit(X_train)
+rescaledX = scaler.transform(X_train)
+model = SVC(C=1.5)
+model.fit(rescaledX, Y_train)
+
+# Estimate accuracy on validation dataset
+rescaledValidationX = scaler.transform(X_validation)
+predictions = model.predict(rescaledValidationX)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+
+
+
